48cd9bbc51f77b02e85ee24319428fee9e84916f
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-26 02:37:17 +0000

    Create Login_form.java

---
+++ app/src/main/java/com/denzo/in_live/data/Login_form.java
@@ -0,0 +1,16 @@
+package com.denzo.in_live.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzo.in_live.R;
+
+public class Login_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+    }
+}
