3b4d0940ecd9264909539ece15d9293906f63f31
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-26 00:50:32 +0000

    Create Signup_Form.java

---
+++ app/src/main/java/com/denzo/in_live/data/Signup_Form.java
@@ -0,0 +1,14 @@
+package com.denzo.in_live.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup__form);
+    }
+}
