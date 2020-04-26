3a8ba2aa417aaf3cdd3a43af2a58467570af8152
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-26 07:12:54 +0000

    Update HomeFragment.java

--- app/src/main/java/com/denzo/in_live/ui/home/HomeFragment.java
+++ app/src/main/java/com/denzo/in_live/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.denzo.in_live.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,23 +15,33 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.denzo.in_live.R;
+import com.denzo.in_live.data.Login_form;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        btnLogout = root.findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(getActivity(), Login_form.class);
+                startActivity(intToMain);
             }
         });
+
         return root;
     }
 }
