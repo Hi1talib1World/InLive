7e24d6daaef3f4252ccbcbe782df7afdf2612adb
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-26 02:27:44 +0000

    Update Signup_Form.java

--- app/src/main/java/com/denzo/in_live/data/Signup_Form.java
+++ app/src/main/java/com/denzo/in_live/data/Signup_Form.java
@@ -12,5 +12,6 @@ public class Signup_Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
+        getSupportActionBar().setTitle("Sign Up !");
     }
 }
