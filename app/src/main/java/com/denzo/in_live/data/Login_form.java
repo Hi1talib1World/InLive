a80c9397e742aee5c2ee423512a0686e97e16411
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-26 04:11:36 +0000

    Update Login_form.java

--- app/src/main/java/com/denzo/in_live/data/Login_form.java
+++ app/src/main/java/com/denzo/in_live/data/Login_form.java
@@ -1,6 +1,7 @@
 package com.denzo.in_live.data;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,11 +12,19 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.denzo.in_live.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login_form extends AppCompatActivity {
 
