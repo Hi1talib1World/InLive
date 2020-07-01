3872d8d4bbad1e77bb0e74fcc9fe447245e366dc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-01 19:30:44 +0100

    Update PhoneAuth.java

--- app/src/main/java/com/denzo/in_live/data/PhoneAuth.java
+++ app/src/main/java/com/denzo/in_live/data/PhoneAuth.java
@@ -4,27 +4,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.denzo.in_live.R;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class PhoneAuth extends AppCompatActivity implements View.OnClickListener {
 
     EditText etPhone, etOtp;
     Button btSendOtp, btResendOtp, btVerifyOtp;
     private FirebaseAuth mAuth;
-
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone__form);
         getSupportActionBar().setTitle("Phone Auth !");
         initFields();
+        mAuth = FirebaseAuth.getInstance();
+
+
 
     }
+    void initFireBaseCallbacks() {
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                Toast.makeText(PhoneAuth.this, "Verification Complete", Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(PhoneAuth.this, "Verification Failed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+                Toast.makeText(PhoneAuth.this, "Code Sent", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
     void initFields() {
         etPhone = findViewById(R.id.et_phone);
         etOtp = findViewById(R.id.et_otp);
