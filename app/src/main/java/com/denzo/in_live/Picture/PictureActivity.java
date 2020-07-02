10b47428f1e6f2762e5872d94248c933acbc8ddf
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-02 05:17:56 +0100

    Create PictureActivity.java

---
+++ app/src/main/java/com/denzo/in_live/Picture/PictureActivity.java
@@ -0,0 +1,16 @@
+package com.denzo.in_live.Picture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzo.in_live.R;
+
+public class PictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+    }
+}
