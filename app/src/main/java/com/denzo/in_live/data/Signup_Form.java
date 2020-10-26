95b610919a393c8fe281196ed400c7b6995af1ac
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-10-26 20:11:14 +0100

    Update Signup_Form.java

--- app/src/main/java/com/denzo/in_live/data/Signup_Form.java
+++ app/src/main/java/com/denzo/in_live/data/Signup_Form.java
@@ -29,6 +29,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup_Form extends AppCompatActivity {
 
