bf08764518cf1d8291e68444ac4e7c9791ed1df5
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-03-07 12:47:08 +0100

    Create FetchZeeDrm.java

---
+++ app/src/main/java/com/denzo/in_live/task/FetchZeeDrm.java
@@ -0,0 +1,74 @@
+package com.denzo.in_live.task;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+
+public class FetchZeeDrm extends AsyncTask<Void,Void,Void> {
+    private Context context;
+    private String licenseUrl;
+    private CallBack callBack;
+    private DrmSessionManager<ExoMediaCrypto> drmSessionManager;
+    public FetchZeeDrm(String licenseUrl,Context context) {
+        this.licenseUrl=licenseUrl;
+        this.context= new WeakReference<>(context).get();
+    }
+
+    public FetchZeeDrm setCallBack(CallBack callBack) {
+        this.callBack = callBack;
+        return this;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        try {
+            ExoMediaDrm.KeyRequest keyRequest=new ExoMediaDrm.KeyRequest(readAsset("content.data"),licenseUrl);
+            HttpMediaDrmCallback drmCallback =
+                    new HttpMediaDrmCallback(licenseUrl, new DefaultHttpDataSourceFactory(agent));
+            drmCallback.executeKeyRequest(Util.getDrmUuid("application/octet-stream"),keyRequest);
+
+            drmSessionManager = new DefaultDrmSessionManager
+                    .Builder()
+                    .build(drmCallback);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        if (callBack!=null)
+            callBack.result(drmSessionManager);
+
+    }
+
+    private byte[] readAsset(String assetName){
+
+        InputStream inputStream ;
+        try {
+            inputStream = context.getAssets().open(assetName);
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+            return output.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new byte[]{};
+        }
+
+    }
+
+    public interface CallBack{
+        void result(DrmSessionManager drmSessionManager);
+    }
+}
