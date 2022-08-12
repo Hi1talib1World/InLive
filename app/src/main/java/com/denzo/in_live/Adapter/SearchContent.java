8a5fdd1ba298ec1346899fef722543f329bb17d1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-08-12 23:38:18 +0100

    Update SearchContent.java

--- app/src/main/java/com/denzo/in_live/Adapter/SearchContent.java
+++ app/src/main/java/com/denzo/in_live/Adapter/SearchContent.java
@@ -1,4 +1,49 @@
 package com.denzo.in_live.Adapter;
 
-public class SearchContent {
+
+import com.denzo.in_live.Activity.SeriesActivity;
+
+public class SearchContent extends RecyclerBuilder<ContentItem> {
+    public SearchContent(Context context, int layout) {
+        super(context, layout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position, @NonNull ContentItem model, View v) {
+        TextView tvCategory=findViewById(R.id.tv_title);
+        ImageView imgThumbnail=findViewById(R.id.img_thumbnail);
+
+        tvCategory.setText(model.getName());
+        if (model.getPosterUrl()!=null){
+            Glide.with(getContext()).load(model.getPosterUrl()).placeholder(shimmer()).into(imgThumbnail);
+        }
+
+    }
+
+    @Override
+    public void onClick(View view, ContentItem model, int id) {
+        if (model.getIsSeries().equals("0") || model.getIsSeries().equals("4")){
+            Fetcher.ref(model.getApiUrl()).setMethod(Method.GET)
+                    .connect(MoviePlayBackModel.class, response -> {
+                        view.setEnabled(true);
+                        if (response.getObject()!=null)
+                        {
+                            Bundle bundle=new Bundle();
+                            bundle.putSerializable("MoviesPlaybackData",response.getObject());
+                            start(MoviesDetailsActivity.class,bundle);
+                        }
+                        else Toast.makeText(getContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+                    });
+        }
+        else if (model.getIsSeries().equals("1")){
+
+            Fetcher.ref(model.getApiUrl()).setMethod(Method.GET).connect(SeriesModel.class, response -> {
+                view.setEnabled(true);
+                SeriesModel seriesModel=response.getObject();
+                Bundle bundle=new Bundle();
+                bundle.putSerializable("SeriesModel",seriesModel);
+                start(SeriesActivity.class,bundle);
+            });
+        }
+    }
 }
