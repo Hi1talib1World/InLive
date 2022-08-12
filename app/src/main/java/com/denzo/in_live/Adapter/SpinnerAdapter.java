6377cfe6f34e6658a4bbf4adbf814ec565e7f390
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-08-12 23:38:11 +0100

    Update SpinnerAdapter.java

--- app/src/main/java/com/denzo/in_live/Adapter/SpinnerAdapter.java
+++ app/src/main/java/com/denzo/in_live/Adapter/SpinnerAdapter.java
@@ -1,4 +1,35 @@
 package com.denzo.in_live.Adapter;
 
-public class SpinnerAdapter {
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.denzo.in_live.Model.Series.SeasonListModel;
+import com.denzo.in_live.R;
+
+public class SpinnerAdapter extends ArrayAdapter<SeasonListModel> {
+
+    LayoutInflater inflate;
+    public SpinnerAdapter(@NonNull Context context, int resource, @NonNull List<SeasonListModel> objects) {
+        super(context, resource, objects);
+        inflate=LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null){
+            convertView = inflate.inflate(R.layout.spinner_style,parent, false);
+        }
+        SeasonListModel rowItem = getItem(position);
+        TextView txtTitle = convertView.findViewById(R.id.title);
+        txtTitle.setText(rowItem.toString());
+        ImageView imageView = convertView.findViewById(R.id.icon);
+        imageView.setImageResource(R.drawable.ic_arrow_drop_down);
+        return convertView;
+    }
 }
