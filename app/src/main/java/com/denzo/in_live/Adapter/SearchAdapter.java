52523ac4f707c63e1c7b262ca5f9d4d87cc04ece
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-08-12 23:38:21 +0100

    Update SearchAdapter.java

--- app/src/main/java/com/denzo/in_live/Adapter/SearchAdapter.java
+++ app/src/main/java/com/denzo/in_live/Adapter/SearchAdapter.java
@@ -1,4 +1,34 @@
 package com.denzo.in_live.Adapter;
 
-public class SearchAdapter {
+
+public class SearchAdapter extends RecyclerBuilder<ContentItem> {
+
+    public SearchAdapter(Context context, int layout) {
+        super(context, layout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position, @NonNull ContentItem model, View v) {
+        TextView tvTitle=findViewById(R.id.tv_title);
+        RecyclerView recyclerView=findViewById(R.id.rv_category);
+        SearchContent adapter=new SearchContent(getContext(),R.layout.holder_home_category);
+        tvTitle.setText(model.getCatName());
+        recyclerView.setLayoutManager(Utils.linear(RecyclerView.HORIZONTAL));
+        recyclerView.setAdapter(adapter);
+
+        List<ContentItem> list=new ArrayList<>();
+        for (ContentItem contentItem : model.getContent()) {
+            contentItem.setCategoryType(model.getCatName());
+            list.add(contentItem);
+        }
+        adapter.setList(list);
+
+    }
+
+    @Override
+    public void onClick(View view, ContentItem model, int id) {
+
+    }
+
+
 }
