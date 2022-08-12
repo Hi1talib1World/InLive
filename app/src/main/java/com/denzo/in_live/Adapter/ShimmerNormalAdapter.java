2c02c59c28290428e9f58fd529ac34538ab2c675
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-08-12 23:38:16 +0100

    Update ShimmerNormalAdapter.java

--- app/src/main/java/com/denzo/in_live/Adapter/ShimmerNormalAdapter.java
+++ app/src/main/java/com/denzo/in_live/Adapter/ShimmerNormalAdapter.java
@@ -1,4 +1,31 @@
 package com.denzo.in_live.Adapter;
 
-public class ShimmerNormalAdapter {
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.denzo.in_live.R;
+
+public class ShimmerNormalAdapter extends RecyclerBuilder {
+    public ShimmerNormalAdapter(Context context, int layout) {
+        super(context, layout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position, @NonNull Object model, View v) {
+        ShimmerFrameLayout shimmerFrameLayout=v.findViewById(R.id.live_fb);
+    }
+
+    public void cleanup(){
+        getArrayList().clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View view, Object model, int id) {
+
+    }
+
 }
