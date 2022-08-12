5da4072d8f46ad4d29b829290f7cc4edf547375e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-08-12 23:38:13 +0100

    Update SliderAdapter.java

--- app/src/main/java/com/denzo/in_live/Adapter/SliderAdapter.java
+++ app/src/main/java/com/denzo/in_live/Adapter/SliderAdapter.java
@@ -1,4 +1,21 @@
 package com.denzo.in_live.Adapter;
 
-public class SliderAdapter {
+
+import com.denzo.in_live.R;
+
+public class SliderAdapter extends RecyclerBuilder<SliderItem> {
+    public SliderAdapter(Context context, int layout) {
+        super(context, layout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder holder, int position, @NonNull SliderItem model, View v) {
+        ImageView imageView=findViewById(R.id.img_slide);
+        Glide.with(getContext()).load(model.getImg()).placeholder(shimmer()).into(imageView);
+    }
+
+    @Override
+    public void onClick(View view, SliderItem model, int id) {
+
+    }
 }
