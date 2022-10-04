7b9ff1afae1e80fa7af3cf7c142be4cd9c70f84d
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-10-04 11:23:25 +0100

    Update MainActivity.java

--- app/src/main/java/com/denzo/in_live/MainActivity.java
+++ app/src/main/java/com/denzo/in_live/MainActivity.java
@@ -3,6 +3,7 @@ package com.denzo.in_live;
 import android.os.Bundle;
 
 import com.denzo.in_live.Model.Update.UpdateModel;
+import com.denzo.in_live.Utils.Constant;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
