d4e3680768eca04502a1f411be7d98035dc44859
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-10-14 02:07:31 +0100

    Update MoreFragment.java

--- app/src/main/java/com/denzo/in_live/ui/more/MoreFragment.java
+++ app/src/main/java/com/denzo/in_live/ui/more/MoreFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.denzo.fetcher.Utils.Utils;
 import com.denzo.in_live.Adapter.MoreAdapter;
 import com.denzo.in_live.BuildConfig;
 import com.denzo.in_live.R;
@@ -26,7 +27,6 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.internal.Utils;
 
 public class MoreFragment extends InitFragment {
 
