8f41fbed8515edcd2cb35912a28b8b84e521d955
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-11-15 09:35:05 +0100

    Update SeasonListModel.java

--- app/src/main/java/com/denzo/in_live/Model/Series/SeasonListModel.java
+++ app/src/main/java/com/denzo/in_live/Model/Series/SeasonListModel.java
@@ -1,4 +1,36 @@
 package com.denzo.in_live.Model.Series;
 
-public class SeasonListModel {
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+import java.io.Serializable;
+
+public class SeasonListModel implements Serializable {
+
+    @SerializedName("season_no")
+    private String seasonNo;
+
+    @SerializedName("episodes")
+    private List<EpisodesItem> episodes;
+
+    public void setSeasonNo(String seasonNo){
+        this.seasonNo = seasonNo;
+    }
+
+    public String getSeasonNo(){
+        return seasonNo;
+    }
+
+    public void setEpisodes(List<EpisodesItem> episodes){
+        this.episodes = episodes;
+    }
+
+    public List<EpisodesItem> getEpisodes(){
+        return episodes;
+    }
+
+    @Override
+    public String toString(){
+        return seasonNo;
+    }
 }
