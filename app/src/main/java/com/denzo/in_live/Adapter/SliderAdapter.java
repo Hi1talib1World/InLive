d1589356d40c12dbb9ddba6b3db30712d4a619cc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-07 02:51:04 +0100

    feat: update SliderAdapter logic

--- app/src/main/java/com/denzo/in_live/Adapter/SliderAdapter.java
+++ app/src/main/java/com/denzo/in_live/Adapter/SliderAdapter.java
@@ -3,6 +3,7 @@ package com.denzo.in_live.Adapter;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import android.content.Context;
@@ -21,7 +22,14 @@ public class SliderAdapter extends RecyclerBuilder<SliderItem> {
     @Override
     public void onBindViewHolder(@NonNull BaseViewHolder holder, int position, @NonNull SliderItem model, View v) {
         ImageView imageView=findViewById(R.id.img_slide);
+        TextView tvTitle=findViewById(R.id.tv_slide_title);
+        TextView tvDesc=findViewById(R.id.tv_slide_desc);
+        
         Glide.with(getContext()).load(model.getImg()).placeholder(shimmer()).into(imageView);
+        if (model.getName() != null) {
+            tvTitle.setText(model.getName());
+        }
+        tvDesc.setVisibility(View.GONE); // Hide desc for now if not available in model
     }
 
     @Override
