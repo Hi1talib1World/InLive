51472120f7125d8053d51fa86f07d5015caa3b07
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-07 02:49:56 +0100

    feat: update SliderItem logic

--- app/src/main/java/com/denzo/in_live/Model/Home/SliderItem.java
+++ app/src/main/java/com/denzo/in_live/Model/Home/SliderItem.java
@@ -7,8 +7,19 @@ public class SliderItem{
     @SerializedName("img")
     private String img;
 
+    @SerializedName("name")
+    private String name;
+
     @SerializedName("api_url")
     private String apiUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void setImg(String img){
         this.img = img;
